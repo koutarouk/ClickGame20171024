ef45ea01c08509dae4cd3da0f53d595852fac1d9
GT20176073\user <nkc.koutarou.kawase@gmail.com>
2017-10-26 10:58:42 +0900

    未完成

---
+++ Assets/BallGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallGenerator : MonoBehaviour {
+
+    public GameObject CubePrefab;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject Cube =
+                Instantiate(CubePrefab) as GameObject;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            Cube.GetComponent<CubeController>().Shoot(
+                worldDir.normalized * 2000);
+        }
+	}
+}
